a9ff801b3a20841be612e85a3de97624e2c0aea3
pablo chesini <pchesini1@gmail.com>
2025-03-28 16:20:27 -0300

    consumir API de la NASA

---
+++ src/utils/fetch.ts
@@ -0,0 +1,17 @@
+const API_KEY = 'LO6ZCq3rz6YaYm2Wc0m1QEn37ILHzWaMGYgylcff';
+const API_URL = 'https://api.nasa.gov/planetary/apod';
+
+export default async (urlParams?: string) => {
+    try{
+        const response = await fetch(
+            `${API_URL}?api_key=${API_KEY}${typeof urlParams !=='undefined' && urlParams?.length > 0
+                 ? urlParams : ''}`,
+        );
+        const data = await response.json();
+        return Promise.resolve(data);
+
+    } catch(error){
+        return Promise.reject(error);
+    };
+    
+}
