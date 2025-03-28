b55f4f8802c3ba4493000ca86e8b57d193e4de86
pablo chesini <pchesini1@gmail.com>
2025-03-28 16:38:26 -0300

    Guardar imagen del día en el estado

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import  Header from "../../componentes/Header";
+import fetchApi from "../../utils/fetch";
 
 const Home = () =>{
+    const [todayImage, setTodayImage] = useState();
+    useEffect (() => {
+        const loadTodayImage = async () =>{
+            try{
+                const todayImageResponse = await fetchApi();
+                setTodayImage(todayImageResponse);
+                //console.log(todayImage);
+            } catch (error)
+            {
+                console.error(error);
+                setTodayImage(undefined);
+            }
+
+        }
+        loadTodayImage().catch(null);
+    }, []);
+
     return (
         <View style={styles.container}>
         <Header/>
