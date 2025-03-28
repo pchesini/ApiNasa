2e7cc3427cb1cf15b4fff76295dd4f4f937fb8a9
pablo chesini <pchesini1@gmail.com>
2025-03-28 15:11:44 -0300

    Se crea Componente head para ser presentado en el home

--- src/componentes/Header.tsx
+++ src/componentes/Header.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 const Header = () => {
     return (
-        <View>
-            <Text>Explora</Text>
+        <View style={styles.headerContainer}>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>Explora</Text>
+            </View>
+            <View style={styles.imageContainer}>
+                <Image source={require('../../assets/logo_nasa.png')} style={styles.logo} />
+            </View>
         </View>
-    )
+    );
 };
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    textContainer: {
+        marginBottom: 10,
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    imageContainer: {
+        width: 200,
+        height: 200,
+    },
+    logo: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain',
+    },
+});
 
 export default Header;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+import  Header from "../../componentes/Header";
+
+const Home = () =>{
+    return (
+        <View>
+        <Header/>
+        </View>
+    )
+}
+export default Home;
